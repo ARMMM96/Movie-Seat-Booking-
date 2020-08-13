0b89456f2eec56ef42cfd98e3e3c942a6b5f396f
Ahmed Rabie <ahmed.rabie.mahmoud.96@gmail.com>
2020-08-13 21:56:44 +0200

    Save Data To Local Storage

--- script.js
+++ script.js
@@ -7,15 +7,34 @@ const moviesSelect = document.querySelector('#movie');
 // PS: the plus sign converting the value from string to number rather than useing parseint function
 let ticketPrice = +moviesSelect.value;
 
+// Save selected movie index and price
+function setMovieData(movieIndex, moviePrice) {
+  localStorage.setItem('selectedMoiveIndex', movieIndex);
+  localStorage.setItem('selectedMovidePrice', moviePrice);
+}
+
 // Update total and count
 function updateSelectedCount() {
   const selectedSeats = document.querySelectorAll('.row .seat.selected');
+
+  /*   
+    1- Copy selected seats in arr
+    2- Map through array
+    3- Return new array indexes
+  */
+  const seatsIndex = [...selectedSeats].map((seat) => [...seats].indexOf(seat));
+
+  localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+
   const selectedSeatsCount = selectedSeats.length;
+
+  count.innerText = selectedSeatsCount;
   total.innerText = selectedSeatsCount * ticketPrice;
 }
 // Movie select event
 moviesSelect.addEventListener('change', (e) => {
   ticketPrice = e.target.value;
+  setMovieData(e.target.selectedIndex, e.target.value);
   updateSelectedCount();
 });
 
